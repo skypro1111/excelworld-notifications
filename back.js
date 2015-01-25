17f64e6c1d875241995cf63e4d297e017c40c58b
skypro1111 <skypro1111@gmail.com>
2015-01-25 19:09:27 +0200

    Обновление после изменения структуры форума + правки отображения уведомлений

--- back.js
+++ back.js
@@ -1,7 +1,6 @@
-﻿document.write('<script src="jquery.js"></script>');
+﻿﻿document.write('<script src="jquery.js"></script>');
 document.write('<script src="popup.js"></script>');
 
-var first = false;
 var id = 0;
 var urlsarr = new Array();
 
@@ -18,34 +17,33 @@ setInterval(function()
             function request(i){
                 var xmlhttp = new XMLHttpRequest();
                 xmlhttp.open("GET", options[i]['url'], false);
-
                 xmlhttp.onreadystatechange = function () {
                     if (xmlhttp.status == 200) {
                         if(options[i]['active'])
                             {
-                                // console.log(options[i]['url']);
-                                $(xmlhttp.responseText).find('td.threadNametd a.threadLink').each(function()
+                                console.log(options[i]['url']);
+                                $(xmlhttp.responseText).find('table.gTable tr[id]').each(function() //td.threadNametd a.threadLink
                                     {
-                                        var urls = $(this).attr('href');
-                                        var text = $(this).text();
-                                        
-                                        // console.log(urls); 
-                                        if(!window.localStorage.getItem(urls))
-                                            {
-                                                if(!first)
-                                                    {
-                                                        chrome.notifications.create((id++).toString(), 
-                                                                                    {type: "basic", title: text, message: urls, iconUrl: "2.png", isClickable: true}, 
-                                                                                    function(){});
-                                                        window.localStorage.setItem(urls, text);
-                                                        urlsarr[urlsarr.length] = urls;
-                                                    }else{ window.localStorage.setItem(urls, text); }   
-                                            }  // 
+                                        var urls = $(this).find('a.threadLink').attr('href');
+                                        var text = $(this).find('a.threadLink').text();
+                                        var date = new Date();
+                                        var trtime = date.toLocaleDateString()+' '+date.toLocaleTimeString();
+                                        var title = options[i]['name'];
+                                        if(urls){
+                                            if(!window.localStorage.getItem(urls))
+                                                {
+                                                    chrome.notifications.create((id++).toString(), 
+                                                                                {type: "basic", title: title, message: text, contextMessage: trtime, iconUrl: "2.png", isClickable: true}, 
+                                                                                function(){});
+                                                    window.localStorage.setItem(urls, text);
+                                                    urlsarr[urlsarr.length] = urls;
+                                                };
+                                            };
                                     });
                             };
                     };
                     i++;
-                    if(i<9){request(i);};
+                    if(i<1){request(i);};
                 };
                 xmlhttp.send(null);
             };
--- popup.js
+++ popup.js
@@ -5,28 +5,28 @@ $(document).ready(function () {
     //при первом запуске расширения создает в локальном хранилище обьект настроек в виде JSON строки
     if(!window.localStorage.getItem('forumoptions')){
         var opt = 
-            {
-                0 : {name: 'Вопросы по Excel' ,                 sub: false, active: false, url : 'http://www.excelworld.ru/forum/2'},
-                1 : {name: 'Вопросы по Excel для Mac' ,         sub: false, active: false, url : 'http://www.excelworld.ru/forum/14'},
-                2 : {name: 'Вопросы по VBA' ,                   sub: false, active: false, url : 'http://www.excelworld.ru/forum/10'},
-                3 : {name: 'Вопросы по VB, VB.net' ,            sub: false, active: false, url : 'http://www.excelworld.ru/forum/15'},
-                4 : {name: 'Excel и другие приложения' ,        sub: false, active: false, url : 'http://www.excelworld.ru/forum/4'},
-                5 : {name: 'Вопросы по Word' ,                  sub: false, active: false, url : 'http://www.excelworld.ru/forum/16'},
-                6 : {name: 'Вопросы по Access' ,                sub: false, active: false, url : 'http://www.excelworld.ru/forum/17'},
-                7 : {name: 'Вопросы по Power Point' ,           sub: false, active: false, url : 'http://www.excelworld.ru/forum/18'},
-                8 : {name: 'Вопросы по Outlook' ,               sub: false, active: false, url : 'http://www.excelworld.ru/forum/19'},
-                9 : {name: 'Вопросы по Visio' ,                 sub: false, active: false, url : 'http://www.excelworld.ru/forum/20'},
-                10 : {name: 'Вопросы по Share Point' ,          sub: false, active: false, url : 'http://www.excelworld.ru/forum/21'},
-                11 : {name: 'Вопросы по Open Office' ,          sub: false, active: false, url : 'http://www.excelworld.ru/forum/22'},
-                12 : {name: 'Вопросы по Google Docs' ,          sub: false, active: false, url : 'http://www.excelworld.ru/forum/23'},
-                13 : {name: 'Вопросы по 1С' ,                   sub: false, active: false, url : 'http://www.excelworld.ru/forum/24'},
-                14 : {name: 'Готовые решения' ,                 sub: false, active: false, url : 'http://www.excelworld.ru/forum/3'},
-                15 : {name: 'Работа / Фриланс' ,                sub: false, active: false, url : 'http://www.excelworld.ru/forum/6'},
-                16 : {name: 'Неформальное общение' ,            sub: false, active: false, url : 'http://www.excelworld.ru/forum/5'},
-                17 : {name: 'Мозговой штурм' ,                  sub: false, active: false, url : 'http://www.excelworld.ru/forum/7'},
-                18 : {name: 'Вопросы по работе форума и сайта' ,sub: false, active: false, url : 'http://www.excelworld.ru/forum/12'},
-                19 : {name: 'Объявления администрации' ,        sub: false, active: false, url : 'http://www.excelworld.ru/forum/13'},
-            };
+                {
+                    0 : {name: 'Вопросы по Excel' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/2'},
+                    1 : {name: 'Вопросы по Excel для Mac' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/14'},
+                    2 : {name: 'Вопросы по VBA' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/10'},
+                    3 : {name: 'Вопросы по VB, VB.net' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/15'},
+                    4 : {name: 'Excel и другие приложения' , sub: 'upper', chk:true, active: false, url : 'http://www.excelworld.ru/forum/4'},
+                    5 : {name: 'Вопросы по Word' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/16'},
+                    6 : {name: 'Вопросы по Access' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/17'},
+                    7 : {name: 'Вопросы по Power Point' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/18'},
+                    8 : {name: 'Вопросы по Outlook' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/19'},
+                    9 : {name: 'Вопросы по Visio' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/20'},
+                    10 : {name: 'Вопросы по Share Point' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/21'},
+                    11 : {name: 'Вопросы по Open Office' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/22'},
+                    12 : {name: 'Вопросы по Google Docs' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/23'},
+                    13 : {name: 'Вопросы по 1С' , sub: 'sub', chk: true, active: false, url : 'http://www.excelworld.ru/forum/24'},
+                    14 : {name: 'Готовые решения' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/3'},
+                    15 : {name: 'Работа / Фриланс' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/6'},
+                    16 : {name: 'Неформальное общение' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/5'},
+                    17 : {name: 'Мозговой штурм' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/7'},
+                    18 : {name: 'Вопросы по работе форума и сайта' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/12'},
+                    19 : {name: 'Объявления администрации' , sub: 'upper', chk: true, active: false, url : 'http://www.excelworld.ru/forum/13'},
+                            };
         window.localStorage.setItem('forumoptions', JSON.stringify(opt));
     };
 
@@ -35,13 +35,24 @@ options = JSON.parse(window.localStorage.getItem('forumoptions'));
 
 // и выводим в попап
     var res = '';
-    for(i = 0; i<9; i++){
-        var ch;
-        if(options[i]['active'] === true){ch = 'checked="checked"';}else{ch = ''};
-        res += '<label><input type="checkbox" '+ch+' id="'+i+'"/><b>'+options[i]['name']+'</b></label></br>';
+    var ch;
+    var sub;
+
+    for(i = 0; i<20; i++){
+        if(options[i]['active'] === true){
+                ch = 'checked="checked"';
+        }else{
+                ch = ''
+            };
+
+        res += '<label class="'+options[i]['sub']+'">';
+        if(options[i]['chk'] === true){
+            res += '<input type="checkbox" '+ch+' id="'+i+'"/>';
+        };
+        res += '<b>'+options[i]['name']+'</b></label></br>';
     }   
-$('#version').html('Обсуждение v '+chrome.app.getDetails().version);
-$('#main1').html(res);
+    $('#version').html('Обсуждение v '+chrome.app.getDetails().version);
+    $('#main1').html(res);
 });
 
     
@@ -50,31 +61,34 @@ $('#main1').html(res);
 
     //отлавливаем нажатие кнопки и сохраняем чекбоксы
         function clickHandler(e) {
-                    first = true;
-        for(var i = 0; i < 9; i++){
-            options[i]['active'] = document.getElementById(i).checked;
+                   // first = true;
+        for(var i = 0; i < 20; i++){
+            if(options[i]['chk'] === true){
+                options[i]['active'] = document.getElementById(i).checked;
+            };
         };
             window.localStorage.setItem('forumoptions', JSON.stringify(options));
-            window.close()
+            window.close();
         }
          
-        document.addEventListener( "DOMContentLoaded" , function () {
-          document.getElementById("btnsave").addEventListener( "click" , clickHandler);
-        });
+            document.addEventListener( "DOMContentLoaded" , function () {
+            document.getElementById("btnsave").addEventListener( "click" , clickHandler);
+                                        });
 
 
 // Удалить пару сообщений для проверки
-        function clickHandler2(e) {
-            // alert('asd');
+        // function clickHandler2(e) {
+            //  alert('asd');
             
-            window.localStorage.removeItem('http://www.excelworld.ru/forum/2-12836-1');
+            // window.localStorage.removeItem('http://www.excelworld.ru/forum/2-15457-1');
+            // window.localStorage.removeItem('asd');
             // console.log(window.localStorage.getItem('http://www.excelworld.ru/forum/10-12711-1'));
-            window.localStorage.removeItem('http://www.excelworld.ru/forum/2-12831-1');
+            // window.localStorage.removeItem('http://www.excelworld.ru/forum/2-12831-1');
             // console.log(window.localStorage.getItem('http://www.excelworld.ru/forum/4-12478-1'));
-            window.localStorage.clear();
-            //window.close()
-        }
-         
-        document.addEventListener( "DOMContentLoaded" , function () {
-          document.getElementById("btnclear").addEventListener( "click" , clickHandler2);
-        });
+        //     window.localStorage.clear();
+        //     window.close();
+        // }
+        
+        // document.addEventListener( "DOMContentLoaded" , function () {
+        //   document.getElementById("btnclear").addEventListener( "click" , clickHandler2);
+        //                             });
